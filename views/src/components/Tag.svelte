c73bb27252fcad146c7360184c80d163a3161ff2
monoko <monoko@riseup.net>
2022-04-08 23:53:20 +0200

    add forgotten files

---
+++ views/src/components/Tag.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let text
+  export let color
+  export let background
+
+  if (background == "purple") {
+    background = 'rgba(204, 49, 124, 0.25)'
+    color = '#CC317C'
+  }
+
+  if (background == "green") {
+    background = 'var(--primary-green-25)'
+    color = 'var(--primary-green)'
+  }
+</script>
+
+<span style="background-color: {background}; color: {color}">{text}</span>
+
+<style>
+  span {
+    border-radius: 8px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0px 8px;
+  }
+</style>
